3a48b88cad7592a05a14f1ad5aa74aa3edbb5fc6
Piyush-1332 <piyush1332@gmail.com>
2020-05-01 18:15:48 +0530

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProductsComponent } from './products/products.component';
+import { ViewWishlistComponent } from './view-wishlist/view-wishlist.component';
+import { HomeComponent } from './home/home.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'product',component:ProductsComponent},
+  {path:'wishlist',component:ViewWishlistComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddWishlistComponent } from './add-wishlist/add-wishlist.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ProductsComponent } from './products/products.component';
+import { ViewWishlistComponent } from './view-wishlist/view-wishlist.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddWishlistComponent,
+    HeaderComponent,
+    FooterComponent,
+    ProductsComponent,
+    ViewWishlistComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
